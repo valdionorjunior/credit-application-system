b3741ddffba9fb324d9799e1388a018c0c3b83e5
valdionorjunior <valdionorjunior@outlook.com>
2023-06-21 16:20:35 -0300

    Add flyway migration for customer and credit, add repository files

---
+++ src/main/kotlin/com/juniorrodrigues/credit/application/system/repository/CustomerRepository.kt
@@ -0,0 +1,8 @@
+package com.juniorrodrigues.credit.application.system.repository
+
+import com.juniorrodrigues.credit.application.system.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long>
