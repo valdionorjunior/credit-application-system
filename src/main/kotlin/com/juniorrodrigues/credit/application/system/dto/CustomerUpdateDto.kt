90ae323961be08dc77ec1e05f1e61cf18561e338
valdionorjunior <valdionorjunior@outlook.com>
2023-06-27 18:48:33 -0300

    Add annotations for validation of dto

--- src/main/kotlin/com/juniorrodrigues/credit/application/system/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/com/juniorrodrigues/credit/application/system/dto/CustomerUpdateDto.kt
@@ -1,17 +1,29 @@
 package com.juniorrodrigues.credit.application.system.dto
 
 import com.juniorrodrigues.credit.application.system.entity.Customer
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 
 data class CustomerUpdateDto(
+
+    @field:NotEmpty(message = "Fist name cannot be empty")
     val firstName: String,
+
+    @field:NotEmpty(message = "Last name cannot be empty")
     val lastName: String,
+
+    @field:NotNull(message = "Income name cannot be Null")
     val income: BigDecimal,
+
+    @field:NotEmpty(message = "Zip Code name cannot be empty")
     val zipCode: String,
+
+    @field:NotEmpty(message = "Street name cannot be empty")
     val street: String
 ) {
 
-    fun toEntity(customer:Customer):Customer{
+    fun toEntity(customer: Customer): Customer {
         customer.firstName = this.firstName
         customer.lastName = this.lastName
         customer.income = this.income
